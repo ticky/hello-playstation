f69064267e91331285568a02a7e571f5136cc93b
Jessica Stokes <hello@jessicastokes.net>
2021-07-15 21:39:27 -0700

    Override all gsKit_fontm_print routines to draw pixel-precise

--- hello-playstation.c
+++ hello-playstation.c
@@ -12,6 +12,13 @@
 
 #include "libpad.h"
 
+// The float coordinates used for the text drawing routines are off by 0.5f,
+// I don't like that, so here I force them to be correct in this program
+#define gsKit_fontm_print_scaled(gsGlobal, gsFontM, X, Y, Z, scale, colour, String) \
+        gsKit_fontm_print_scaled(gsGlobal, gsFontM, 0.5f + X, 0.5f + Y, Z, scale, colour, String)
+// Note that gsKit_fontm_print calls gsKit_fontm_print_scaled under the hood,
+// so this adjusts both
+
 GSGLOBAL *gsGlobal;
 GSFONTM *gsFontM;
 
